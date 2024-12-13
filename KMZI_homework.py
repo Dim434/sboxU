77d087e08eb8704f653ee0932c436b30947f839b
ddif <dmitry@devyy.io>
2024-12-13 15:39:33 +0300

    KMZI homework

---
+++ KMZI_homework.py
@@ -0,0 +1,29 @@
+#!/usr/bin/sage
+
+from sage.all import *
+from sboxU import *
+import time
+
+
+
+if __name__ == '__main__':
+    S_boxes = [
+        [6, 11, 14, 0, 15, 1, 4, 3, 10, 13, 2, 8, 12, 7, 5, 9],
+        [4, 6, 3, 8, 12, 2, 0, 10, 13, 9, 14, 11, 5, 15, 7, 1],
+        [13, 1, 8, 9, 6, 14, 11, 3, 12, 4, 15, 5, 7, 2, 0, 10],
+        [6, 14, 4, 10, 15, 11, 5, 8, 2, 0, 1, 9, 13, 3, 7, 12]
+    ]
+    print(differential_spectrum(S_boxes[0]))
+    for i, S in enumerate(S_boxes, start=1):
+        DDT = ddt(S)
+        LAT = lat(S)
+        # Дифференциальная равномерность
+        print(DDT)
+        print(LAT)
+        diff_uniformity = max(DDT[a][b] for a in range(1, len(S)) for b in range(len(S))) / (2 ** 4)
+
+        # Линейная равномерность(не уврен: что правильно)
+        lin_uniformity = max(abs(LAT[u][v]) for u in range(1, len(S)) for v in range(len(S))) / (2 ** 4)
+        print("S[{}] Diff: {}, Lin: {}".format(i, diff_uniformity, lin_uniformity))
+        for j, SS in enumerate(S_boxes, start=1):
+            print("Affine equivalency S[{}] and S[{}]: {}".format(i, j, len(affine_equivalence(S, SS)) != 0))
